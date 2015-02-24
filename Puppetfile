35a97e981aad1de44c699da6416fbbd0ecb9e0c4
Arnd Hannemann <arnd.hannemann@credativ.de>
2015-02-24 11:34:21 +0100

    Update puppetlabs/lvm to upstream version 0.4.0, it now supports mkfs_options

--- Puppetfile
+++ Puppetfile
@@ -5,10 +5,7 @@ mod 'puppetlabs/apt', '1.5.0'
 mod 'puppetlabs/apache', '0.0.4'
 mod 'puppetlabs/postgresql', '3.3.3'
 mod 'puppetlabs/concat', '1.0.2'
-
-mod 'lvm',
-  :git => 'https://github.com/credativ/puppetlabs-lvm.git',
-  :ref => '0.3.3+cred1'
+mod 'puppetlabs/lvm', '0.4.0'
 
 # mod 'ntp',
 #   :git => 'git://github.com/puppetlabs/puppetlabs-ntp.git'
--- modules/lvm/manifests/logical_volume.pp
+++ modules/lvm/manifests/logical_volume.pp
@@ -1,14 +1,21 @@
-define lvm::logical_volume(
+# == Define: lvm::logical_volume
+#
+define lvm::logical_volume (
   $volume_group,
   $size,
+  $initial_size      = undef,
   $ensure            = present,
   $options           = 'defaults',
   $fs_type           = 'ext4',
   $mkfs_options      = undef,
-  $stripes           = undef,
   $mountpath         = "/${name}",
   $mountpath_require = false,
+  $extents           = undef,
+  $stripes           = undef,
+  $stripesize        = undef,
+  $range             = undef,
 ) {
+
   validate_bool($mountpath_require)
 
   if $mountpath_require {
@@ -36,10 +43,14 @@ define lvm::logical_volume(
     ensure       => $ensure,
     volume_group => $volume_group,
     size         => $size,
+    initial_size => $initial_size,
     stripes      => $stripes,
+    stripesize   => $stripesize,
+    extents      => $extents,
+    range        => $range,
   }
 
-  filesystem {"/dev/${volume_group}/${name}":
+  filesystem { "/dev/${volume_group}/${name}":
     ensure  => $ensure,
     fs_type => $fs_type,
     options => $mkfs_options,
@@ -50,7 +61,7 @@ define lvm::logical_volume(
     command => "mkdir -p ${mountpath}",
     unless  => "test -d ${mountpath}",
   } ->
-  mount {$mountpath:
+  mount { $mountpath:
     ensure  => $mount_ensure,
     device  => "/dev/${volume_group}/${name}",
     fstype  => $fs_type,
@@ -59,5 +70,4 @@ define lvm::logical_volume(
     dump    => 1,
     atboot  => true,
   }
-
 }
